42f77991e22bcd6c55c42829a8a443d1657fd1ab
rinaga <olahjennifer@hotmail.com>
2019-03-11 16:56:29 -0700

    bla

---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './hello.css';
+
+class Hello extends Component {
+    render() {
+        return (
+            <div class='tc ph4 f2'>
+                <h1>Hello</h1>
+                <p>blablala</p>
+            </div>
+        );
+    }
+}
+export default Hello;
